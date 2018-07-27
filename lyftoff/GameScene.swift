c1d3988d38841f73131062ffe78f648700df7ad1
Nicolas Magana <nmagana@lyft.com>
2018-07-26 22:22:50 -0700

    Rendered scoreboard and player

--- lyftoff/GameScene.swift
+++ lyftoff/GameScene.swift
@@ -9,17 +9,47 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var starfield:SKEmitterNode!
     var player:SKSpriteNode!
+    var scoreLabel:SKLabelNode!
+    var score:Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     
     override func didMove(to view: SKView) {
+        self.anchorPoint = CGPoint(x:0, y:0)
+        // Initializes Starfield object
         starfield = SKEmitterNode(fileNamed: "Starfield")
-        starfield.position = CGPoint(x: 0, y: 1472)
+        // Sets Position to top left of screen
+        starfield.position = CGPoint(x: self.frame.size.width / 2, y: 1472)
+        // advances time to already be falling
         starfield.advanceSimulationTime(10)
+        // Creates Startfield
         self.addChild(starfield)
+        // Sets position of starfield to back
         self.zPosition = -1
+        
+        player = SKSpriteNode(imageNamed: "shuttle")
+        player.position = CGPoint(x: self.frame.size.width / 2, y: player.size.height / 2 + 20)
+        self.addChild(player)
+        
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
+        
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.position = CGPoint(x: 100, y: self.frame.size.height - 60)
+        scoreLabel.fontName = "AmericanTypewriter-Bold"
+        scoreLabel.fontSize = 36
+        scoreLabel.fontColor = UIColor.white
+        score = 0
+        
+        self.addChild(scoreLabel)
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
